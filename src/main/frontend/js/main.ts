8d0b8b146b46f725006271812e242a6adf0d72cb
Clemens Grabmann <6975408+cgrabmann@users.noreply.github.com>
2019-02-03 01:27:19 +0100

    added frontendbuild to gradle

--- src/main/frontend/js/main.ts
+++ src/main/frontend/js/main.ts
@@ -4,12 +4,19 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import Test from './vue/Test.vue'
+import {User} from '@api'
 
 Vue.use(Vuetify, {iconfont: 'md'});
 
 
 new Vue({
     el: '#app',
+    data: {
+        user: {
+            id: 10,
+            name: 'test'
+        } as User
+    },
     render(r) {
         return r(Test, {});
     }
