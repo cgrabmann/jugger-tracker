af05e28d6482c4af339f08c199307f450dfec3ab
Dominik Dopplinger <dom.dopplinger@gmail.com>
2019-03-19 00:45:21 +0100

    Implemented insecure user mapper

--- src/main/java/at/jugger/tracker/mapper/UserMapper.java
+++ src/main/java/at/jugger/tracker/mapper/UserMapper.java
@@ -4,32 +4,36 @@ import at.jugger.tracker.config.MapperConfig;
 import at.jugger.tracker.domain.UserEntity;
 import at.jugger.tracker.dto.User;
 import at.jugger.tracker.dto.UserData;
-import at.jugger.tracker.mapper.decorator.RoleDependendUserMapperDecorator;
 import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(config = MapperConfig.class)
-@DecoratedWith(RoleDependendUserMapperDecorator.class)
 public interface UserMapper {
 
     @Mappings({
-            @Mapping(target = "id", source = "userId")
+            @Mapping(target = "id", source = "userId"),
+            @Mapping(target = "email", source = "email", ignore = true),
+            @Mapping(target = "role", source = "role", ignore = true)
     })
     User toDto(UserEntity entity);
 
     List<User> toDtos(List<UserEntity> entities);
 
-    @Mapping(target = "trackable", defaultValue = "false")
-    @Mappings(
-            @Mapping(target = "userId", ignore = true)
-    )
+    @Mappings({
+            @Mapping(target = "userId", ignore = true),
+            @Mapping(target = "email", source = "email", ignore = true),
+            @Mapping(target = "role", source = "role", ignore = true),
+            @Mapping(target = "trackable", defaultValue = "false")
+    })
     UserEntity toEntity(UserData user, @MappingTarget UserEntity entity);
 
-    @Mapping(target = "trackable", defaultValue = "false")
-    @Mappings(
-            @Mapping(target = "userId", ignore = true)
-    )
+    @Mappings({
+            @Mapping(target = "userId", ignore = true),
+            @Mapping(target = "email", source = "email", ignore = true),
+            @Mapping(target = "role", source = "role", ignore = true),
+            @Mapping(target = "trackable", defaultValue = "false")
+    })
     UserEntity toEntity(UserData newUser);
 
     @InheritInverseConfiguration
